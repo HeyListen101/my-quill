39e822b348114658d50e3adfb34cb90887aec242
Jose Rafael S. Matienzo <josematienzo101@gmail.com>
2025-04-22 21:29:43 +0800

    Apply UI design on authentication pages

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,9 +1,22 @@
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
-  );
+import Image from "next/image"
+import icon from "@/public/icon.png"
+
+export default function AuthLayout({ children } : { children : React.ReactNode}) {
+    return (
+        <main className="size-full">
+            <div
+                className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center min-h-screen filter blur-sm scale-105"
+                aria-hidden="true"
+            />
+            <div className="flex justify-center items-center size-full">
+                <div className="flex flex-col relative w-[400px] h-[330px] bg-white rounded-[20px] px-[40px] py-[30px] shadow-md gap-[25px]">
+                    <div className="flex gap-[3px] items-center">
+                        <Image src={icon} alt="My Quill Icon" className="size-[24px]" />
+                        <span>My Quill</span>
+                    </div>
+                    { children }
+                </div>
+            </div>
+        </main>
+    )
 }
